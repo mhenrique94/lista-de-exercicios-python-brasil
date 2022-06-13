1cf998b74df979282deea1a26a1665d50701fed2
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-13 17:42:04 -0300

    Resolvido exercício 09

--- secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
+++ secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
@@ -12,3 +12,10 @@ Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50
 
 def calcular_numeros_impares_de_1_a_50() -> str:
     """Escreva aqui em baixo a sua solução"""
+    cont = 1
+    texto = []
+    while cont < 51:
+        if cont % 2 != 0:
+            texto.append(cont)
+        cont = cont +1
+    print(str(texto).replace('[', "'").replace(']', "'"))
