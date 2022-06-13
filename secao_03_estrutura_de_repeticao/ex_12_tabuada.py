11a95c037a7f6e7af9ee67c3696d156a7a0ebb73
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-13 19:59:52 -0300

    Resolvido exercício 12

--- secao_03_estrutura_de_repeticao/ex_12_tabuada.py
+++ secao_03_estrutura_de_repeticao/ex_12_tabuada.py
@@ -124,3 +124,11 @@ Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número i
 
 def gerar_tabuada(n: int) -> str:
     """Escreva aqui em baixo a sua solução"""
+    cont = 1
+    if n <= 0 or n > 10:
+        print('Somente pode ser gerada tabuada de 1 a 10')
+    else:
+        while cont < 11:
+            print(f'{n} X {cont} = {n * cont}')
+            cont= cont + 1
+
