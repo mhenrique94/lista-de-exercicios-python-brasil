765c861bd633f2b9d47785dfefb0e8c52587d690
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2022-06-20 12:36:30 -0300

    Resolvido exercício 24

--- secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
+++ secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
@@ -16,16 +16,19 @@ Faça um programa que calcule o mostre a média aritmética de N notas.
 """
 
 
-from statistics import mean
+from statistics import StatisticsError, mean
 
 
 def calcular_media(*notas) -> float:
     """Escreva aqui em baixo a sua solução"""
     lista = []
-    for arg in notas:
-        if arg == "":
-            return("'É necessária ao menos uma nota para calcular a média'")
-        else:
+
+    try:
+        for arg in notas:
             lista.append(arg)
+        print(mean(lista))
+
+    except StatisticsError:
+        return('É necessária ao menos uma nota para calcular a média')
         
-    print(mean(lista))
+   
